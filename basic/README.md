64b23d4a4b5d7332413c2d80560c190e5a59be05
Eles <1494136313@qq.com>
2025-12-24 20:12:56 +0800

    chore: update serializers example

---
+++ basic/README.md
@@ -0,0 +1,10 @@
+# Basic
+
+## 最小应用
+
+```bash
+uv sync
+python manage.py migrate
+python manage.py createsuperuser --email admin@example.com --username admin
+python manage.py runserver
+```
--- basic/src/urls.py
+++ basic/src/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
+from src.core import views as core_views
+
 router = routers.DefaultRouter()
+router.register(r"users", core_views.UserViewSet)
+router.register(r"groups", core_views.GroupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
